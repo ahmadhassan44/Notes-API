db38e6f21a14c95b1de7f4ec2afe31841a85cd38
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-06-07 17:43:09 +0500

    mongo db introduction
    created a mongo db cluster connnected with in code defined models for note and user objects

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ const express=require("express");
 const userRouter = require("./routes/userRoutes");
 const noteRouter = require("./routes/noteRoutes");
 const app=express();
-
+const mongoose=require("mongoose");
 
 app.use("/users",userRouter);
 app.use("/notes",noteRouter);
-app.listen(5000,()=>{
-    console.log("Server running in PORT:5000");
+
+
+mongoose
+.connect("mongodb+srv://admin:ahmad123@atlascluster.5w0s17v.mongodb.net/")
+.then(()=>{
+    console.log("Database connection established");
+    app.listen(5000,()=>{
+        console.log("Server running in PORT:5000");
+    });
 })
+.catch((console.error()))
