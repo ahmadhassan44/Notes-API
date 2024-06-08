a6364d7d4c51f25c9c0d34704e895e5277a32826
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-06-08 18:33:41 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const userRouter = require("./routes/userRoutes");
 const noteRouter = require("./routes/noteRoutes");
 const mongoose = require("mongoose");
 const dotenv=require("dotenv");
+const cors=require("cors");
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
